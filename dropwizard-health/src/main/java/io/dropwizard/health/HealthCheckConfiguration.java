878e6d39c563162e52d827dcd64bf7dfcc4f7ff5
Yang Jiang <jyang96069@gmail.com>
2022-09-25 21:50:00 -0500

    add ctest logs to get and set methods

--- dropwizard-health/src/main/java/io/dropwizard/health/HealthCheckConfiguration.java
+++ dropwizard-health/src/main/java/io/dropwizard/health/HealthCheckConfiguration.java
@@ -5,8 +5,14 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class HealthCheckConfiguration {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DefaultHealthFactory.class);
+
+    private static final String CTEST_GET_PARAM_FORMAT = "[CTEST][GET-PARAM] healthChecks.{}";
+    private static final String CTEST_SET_PARAM_FORMAT = "[CTEST][SET-PARAM] healthChecks.{}";
 
     @NotNull
     @Size(min = 1)
@@ -28,35 +34,59 @@ public class HealthCheckConfiguration {
     @JsonProperty
     private Schedule schedule = new Schedule();
 
+    private String getStackTrace() {
+        String stacktrace = " ";
+        for (StackTraceElement element : Thread.currentThread().getStackTrace()) {
+            stacktrace = stacktrace.concat(element.getClassName() + "\t");
+        }
+        return stacktrace;
+    }
+
     public String getName() {
+        String ctestParam = "name";
+        LOGGER.warn(CTEST_GET_PARAM_FORMAT, ctestParam);
         return name;
     }
 
     public void setName(final String name) {
+        String ctestParam = "name";
+        LOGGER.warn(CTEST_SET_PARAM_FORMAT, ctestParam + getStackTrace());
         this.name = name;
     }
 
     public HealthCheckType getType() {
+        String ctestParam = "type";
+        LOGGER.warn(CTEST_GET_PARAM_FORMAT, ctestParam);
         return type;
     }
 
     public void setType(HealthCheckType type) {
+        String ctestParam = "type";
+        LOGGER.warn(CTEST_SET_PARAM_FORMAT, ctestParam + getStackTrace());
         this.type = type;
     }
 
     public boolean isCritical() {
+        String ctestParam = "critical";
+        LOGGER.warn(CTEST_GET_PARAM_FORMAT, ctestParam);
         return critical;
     }
 
     public void setCritical(final boolean critical) {
+        String ctestParam = "critical";
+        LOGGER.warn(CTEST_SET_PARAM_FORMAT, ctestParam + getStackTrace());
         this.critical = critical;
     }
 
     public boolean isInitialState() {
+        String ctestParam = "initialState";
+        LOGGER.warn(CTEST_GET_PARAM_FORMAT, ctestParam);
         return initialState;
     }
 
     public void setInitialState(boolean initialState) {
+        String ctestParam = "initialState";
+        LOGGER.warn(CTEST_SET_PARAM_FORMAT, ctestParam + getStackTrace());
         this.initialState = initialState;
     }
 
--- dropwizard-health/src/main/java/io/dropwizard/health/Schedule.java
+++ dropwizard-health/src/main/java/io/dropwizard/health/Schedule.java
@@ -7,8 +7,14 @@ import javax.annotation.Nullable;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import java.util.Objects;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Schedule {
+    private static final Logger LOGGER = LoggerFactory.getLogger(DefaultHealthFactory.class);
+
+    private static final String CTEST_GET_PARAM_FORMAT = "[CTEST][GET-PARAM] healthChecks.schedule.{}";
+    private static final String CTEST_SET_PARAM_FORMAT = "[CTEST][SET-PARAM] healthChecks.schedule.{}";
 
     @Nullable
     @JsonProperty
@@ -30,44 +36,73 @@ public class Schedule {
     @JsonProperty
     private int successAttempts = 2;
 
+    private String getStackTrace() {
+        String stacktrace = " ";
+        for (StackTraceElement element : Thread.currentThread().getStackTrace()) {
+            stacktrace = stacktrace.concat(element.getClassName() + "\t");
+        }
+        return stacktrace;
+    }
+
     public Duration getInitialDelay() {
+        String ctestParam = "initialDelay";
+        LOGGER.warn(CTEST_GET_PARAM_FORMAT, ctestParam);
+
         // default to checkInterval value
         return initialDelay == null ? getCheckInterval() : initialDelay;
     }
 
     public void setInitialDelay(Duration initialDelay) {
+        String ctestParam = "initialDelay";
+        LOGGER.warn(CTEST_SET_PARAM_FORMAT, ctestParam + getStackTrace());
         this.initialDelay = initialDelay;
     }
 
     public Duration getCheckInterval() {
+        String ctestParam = "checkInterval";
+        LOGGER.warn(CTEST_GET_PARAM_FORMAT, ctestParam);
         return checkInterval;
     }
 
     public void setCheckInterval(final Duration checkInterval) {
+        String ctestParam = "checkInterval";
+        LOGGER.warn(CTEST_SET_PARAM_FORMAT, ctestParam + getStackTrace());
         this.checkInterval = checkInterval;
     }
 
     public Duration getDowntimeInterval() {
+        String ctestParam = "downtimeInterval";
+        LOGGER.warn(CTEST_GET_PARAM_FORMAT, ctestParam);
         return downtimeInterval;
     }
 
     public void setDowntimeInterval(final Duration downtimeInterval) {
+        String ctestParam = "downtimeInterval";
+        LOGGER.warn(CTEST_SET_PARAM_FORMAT, ctestParam + getStackTrace());
         this.downtimeInterval = downtimeInterval;
     }
 
     public int getFailureAttempts() {
+        String ctestParam = "failureAttempts";
+        LOGGER.warn(CTEST_GET_PARAM_FORMAT, ctestParam);
         return failureAttempts;
     }
 
     public void setFailureAttempts(final int failureAttempts) {
+        String ctestParam = "failureAttempts";
+        LOGGER.warn(CTEST_SET_PARAM_FORMAT, ctestParam + getStackTrace());
         this.failureAttempts = failureAttempts;
     }
 
     public int getSuccessAttempts() {
+        String ctestParam = "successAttempts";
+        LOGGER.warn(CTEST_GET_PARAM_FORMAT, ctestParam);
         return successAttempts;
     }
 
     public void setSuccessAttempts(final int successAttempts) {
+        String ctestParam = "successAttempts";
+        LOGGER.warn(CTEST_SET_PARAM_FORMAT, ctestParam + getStackTrace());
         this.successAttempts = successAttempts;
     }
 

a94a3030433bae4e76f735047ab97368380a3689
Yang Jiang <jyang96069@gmail.com>
2022-12-08 11:27:03 -0600

    ctest logging

--- dropwizard-health/src/main/java/io/dropwizard/health/response/ServletHealthResponderFactory.java
+++ dropwizard-health/src/main/java/io/dropwizard/health/response/ServletHealthResponderFactory.java
@@ -6,6 +6,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dropwizard.health.HealthEnvironment;
 import io.dropwizard.jersey.setup.JerseyEnvironment;
 import io.dropwizard.jetty.setup.ServletEnvironment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Collection;
 
@@ -14,6 +16,11 @@ import java.util.Collection;
  */
 @JsonTypeName("servlet")
 public class ServletHealthResponderFactory implements HealthResponderFactory {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ServletHealthResponderFactory.class);
+
+    private static final String CTEST_GET_PARAM_FORMAT = "[CTEST][GET-PARAM] health.responder.{}";
+    private static final String CTEST_SET_PARAM_FORMAT = "[CTEST][SET-PARAM] health.responder.{}";
+
     static final String SERVLET_SUFFIX = "-servlet";
 
     @JsonProperty
@@ -22,19 +29,35 @@ public class ServletHealthResponderFactory implements HealthResponderFactory {
     @JsonProperty
     private String cacheControlValue = "no-store";
 
+    private String getStackTrace() {
+        String stacktrace = " ";
+        for (StackTraceElement element : Thread.currentThread().getStackTrace()) {
+            stacktrace = stacktrace.concat(element.getClassName() + "\t");
+        }
+        return stacktrace;
+    }
+
     public boolean isCacheControlEnabled() {
+        String ctestParam = "cacheControlEnabled";
+        LOGGER.warn(CTEST_GET_PARAM_FORMAT, ctestParam);
         return cacheControlEnabled;
     }
 
     public void setCacheControlEnabled(boolean cacheControlEnabled) {
+        String ctestParam = "cacheControlEnabled";
+        LOGGER.warn(CTEST_SET_PARAM_FORMAT, ctestParam + getStackTrace());
         this.cacheControlEnabled = cacheControlEnabled;
     }
 
     public String getCacheControlValue() {
+        String ctestParam = "cacheControlValue";
+        LOGGER.warn(CTEST_GET_PARAM_FORMAT, ctestParam);
         return cacheControlValue;
     }
 
     public void setCacheControlValue(String cacheControlValue) {
+        String ctestParam = "cacheControlValue";
+        LOGGER.warn(CTEST_SET_PARAM_FORMAT, ctestParam + getStackTrace());
         this.cacheControlValue = cacheControlValue;
     }
 
